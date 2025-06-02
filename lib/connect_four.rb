9006a4e867ee19098c64d0ed1cddab0a43d003b2
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-06-02 22:52:18 +0100

    feat: Setup rspec env, add project scope

---
+++ lib/connect_four.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+# Connect Four Module
+module ConnectFour
+  # ConnectFour::Board class
+  class Board
+    # @todo generate a 7 x 6 board
+
+    def initialize(col = 7, row = 6)
+      @col = col
+      @row = row
+    end
+
+    def generate_board
+      board = Array.new(@row) { Array.new(@col, 0) }
+      p board.flatten.size
+      board
+    end
+  end
+
+  # ConnectFour::Controller clas
+  class Controller
+    # @todo console input control
+  end
+
+  # ConnectFour::Player class
+  class Player
+    # @todo create player
+  end
+end
+
+# core game loop
+# display a grid that's 7-column and 6-row
+# prompt user1 for an input between 1-7, where 1 is the first column on the left, and 7 is the last column on the right
+# when a valid input is received, update the column to reflect user's choice.
+# print a new display to reflect the changes
+# user2 or npc will take their turn
+# repeat the column update process
+# the game ends when a player placed four discs that are connected either vertically, horizontally or diagonally.
+# announce winner
+# prompt restart
+# keyword: exit - exit program
+# keyword: help - print how to play tooltip
