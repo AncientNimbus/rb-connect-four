b30924d8b709b31a940da40f01f7dc1cd2b6b6d7
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-06-05 22:27:04 +0100

    feat: Add text to textfile

--- lib/console.rb
+++ lib/console.rb
@@ -1,13 +1,30 @@
 # frozen_string_literal: true
 
+require_relative "file_utils"
+
+F = FileUtils
+
 module ConsoleGame
   # Game display & input manager for console game
   class Console
+    # prompt message helper
+    # @param msg [String] message to print
+    # @param pre [String] message prefix
+    # @param suf [String] message suffix
+    # @param mode [Symbol] expecting the following symbols: `puts`, `print`, `p`
+    def msg_printer(msg = "Using message printer", pre: "", suf: "", mode: :puts)
+      return ArgumentError("Invalid mode used for this method") unless %i[puts print p].include?(mode)
+
+      formatted_msg = pre + msg + suf
+      method(mode).call(formatted_msg)
+    end
   end
 
   # Game menu manager for console game
   class ConsoleMenu
-    def show_menu
+    # Display the console menu
+    def show
+      F.s("console.menu")
     end
   end
 end
--- lib/game_manager.rb
+++ lib/game_manager.rb
@@ -8,9 +8,11 @@ require_relative "player"
 module ConsoleGame
   # Game Manager for Console game
   class GameManager
+    attr_reader :menu, :cli
+
     def initialize
       @menu = ConsoleMenu.new
-      @console = Console.new
+      @cli = Console.new
       @p1 = Player.new
       @running = true
       @current_game = nil
@@ -19,7 +21,8 @@ module ConsoleGame
     # run the console game manager
     def run
       puts "console is running"
-      puts "loading menu"
+      # puts menu.show
+      puts cli.msg_printer(menu.show)
       puts "hit any key to exit the program"
       gets.chomp
       puts "~~bye~~"
