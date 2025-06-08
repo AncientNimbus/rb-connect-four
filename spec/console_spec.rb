b4707a84a4bc470f37dd4c9ef6c2bc673e4903d1
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-06-08 15:38:38 +0100

    test: Add test suite for ConsoleGame module

--- spec/console_spec.rb
+++ spec/console_spec.rb
@@ -5,4 +5,12 @@ require_relative "./lib/console"
 describe ConsoleGame::Console do
   describe "#prompt_user" do
   end
+
+  describe "msg_printer" do
+  end
+end
+
+describe ConsoleGame::ConsoleMenu do
+  describe "#show" do
+  end
 end
