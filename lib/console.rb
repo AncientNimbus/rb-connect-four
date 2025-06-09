d549c33a408b6e22dc43bc95dca90f499ae0becb
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-06-09 16:25:35 +0100

    feat: Add #command?, #help and #play
    refactor: #prompt_user and #print_msg to access message externally

--- lib/console.rb
+++ lib/console.rb
@@ -8,15 +8,17 @@ module ConsoleGame
   # Game display & input manager for console game
   class Console
     # prompt user to collect input
-    # @param reg [Regexp] patter to match
     # @param msg [String] first print
     # @param err_msg [String] second print
-    def prompt_user(reg = /.*/, msg = "Provide an input: ", err_msg = "Invalid input, please try again: ")
+    # @param reg [Regexp] pattern to match
+    # @param allow_empty [Boolean] allow empty input value, default to false
+    # @return [String] user input
+    def prompt_user(msg = F.s("console.msg.std"), err_msg: F.s("console.msg.err"), reg: /.*/, allow_empty: false)
       input = ""
       loop do
-        msg_printer(msg)
+        print_msg(msg, mode: :print)
         input = gets.chomp
-        break if input.match?(reg) && !input.empty?
+        break if input.match?(reg) && (!input.empty? || allow_empty)
 
         msg = err_msg
       end
@@ -28,19 +30,36 @@ module ConsoleGame
     # @param pre [String] message prefix
     # @param suf [String] message suffix
     # @param mode [Symbol] expecting the following symbols: `puts`, `print`, `p`
-    def msg_printer(msg = "Using message printer", pre: "", suf: "", mode: :puts)
+    def print_msg(msg = "Using message printer", pre: "", suf: "", mode: :puts)
       return ArgumentError("Invalid mode used for this method") unless %i[puts print p].include?(mode)
 
       formatted_msg = pre + msg + suf
       method(mode).call(formatted_msg)
     end
+
+    # returns true if user input matches available commands
+    # @param input [String] user input
+    # @param commands [Array<String>] command string keys
+    # @return [Boolean] whether it is a command or not
+    def command?(input, commands = ["exit"])
+      commands.include?(input.delete_prefix("-")) || commands.include?(input.delete_prefix("--"))
+    end
   end
 
   # Game menu manager for console game
   class ConsoleMenu
+    def initialize
+      @commands = { "exit" => method(:exit), "help" => method(:help), "play" => method(:play) }
+    end
+
     # Display the console menu
-    def show
+    def help
       F.s("console.menu")
+    end
+
+    # Launch a game
+    def play
+      F.s("Launching a game")
     end
   end
 end
