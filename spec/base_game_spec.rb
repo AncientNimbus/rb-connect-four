236f9e7454aa9bf5312e35244e591146a94d714f
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-06-10 18:25:17 +0100

    test: Add test suite for base_game

---
+++ spec/base_game_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require_relative "../lib/base_game"
+
+describe ConsoleGame::BaseGame do
+  context "when game_manager is provided" do
+    let(:mock_player) { double("Player") }
+    let(:mock_game_manager) { double("GameManager", p1: mock_player) }
+    let(:custom_title) { "Custom Game" }
+    subject(:game) { described_class.new(mock_game_manager, custom_title) }
+
+    it "sets the game_manager" do
+      expect(game.game_manager).to eq(mock_game_manager)
+    end
+
+    it "sets the title" do
+      expect(game.title).to eq(custom_title)
+    end
+
+    it "sets the player to game_manager.p1" do
+      expect(game.player).to eq(mock_player)
+    end
+
+    it "sets the initial state to :created" do
+      expect(game.state).to eq(:created)
+    end
+  end
+
+  context "when game_manager is not provided" do
+    subject(:game) { described_class.new }
+
+    it "sets the game_manager to nil" do
+      expect(game.game_manager).to be_nil
+    end
+
+    it "sets the title to default" do
+      expect(game.title).to eq("Base Game")
+    end
+
+    it "sets the player to nil" do
+      expect(game.player).to be_nil
+    end
+
+    it "sets the initial state to :created" do
+      expect(game.state).to eq(:created)
+    end
+  end
+end
--- spec/console_spec.rb
+++ spec/console_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative "./lib/console"
+require_relative "../lib/console"
 
 describe ConsoleGame::Console do
   describe "#prompt_user" do
