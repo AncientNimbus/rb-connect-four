36178e99acf4076bfe326e6b89830885bbabc877
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-06-16 11:33:52 +0100

    refactor: Optimise connect_four class game flow
    refactor: cleanup #initialize method, #play, #player_profile & #setup_game
    refactor: Rewrite the #remaining_slots method with a better variable
    options

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -11,7 +11,7 @@ module ConsoleGame
   # Connect Four the game
   class ConnectFour < BaseGame
     include ConnectFourLogic
-    INFO = { title: "Connect 4", ver: "v0.7.9" }.freeze
+    INFO = { title: "Connect 4", ver: "v0.8.1" }.freeze
 
     attr_reader :p1, :p2, :bound, :combinations, :board_cap, :board_low, :sep, :e_slot, :f_slot, :empty_slots
     attr_accessor :board, :mode, :p1_turn, :has_winner
@@ -22,16 +22,12 @@ module ConsoleGame
       @p2 = nil
       @has_winner = false
 
-      # Board asset: cap
-      @board_cap ||= fetch_board_asset("cap")
-      # Board asset: low
-      @board_low ||= fetch_board_asset("low")
-      # Board asset: separator
-      @sep ||= fetch_board_asset("sep")
-      # Board asset: empty slot
-      @e_slot ||= fetch_board_asset("hollow")
-      # Board asset: filled slot
-      @f_slot ||= fetch_board_asset("filled")
+      @bound = [7, 6]
+      @board_cap = fetch_board_asset("cap")
+      @board_low = fetch_board_asset("low")
+      @sep = fetch_board_asset("sep")
+      @e_slot = fetch_board_asset("hollow")
+      @f_slot = fetch_board_asset("filled")
     end
 
     # Fetch board asset from textfile
@@ -41,21 +37,20 @@ module ConsoleGame
 
     # Sequence to setup game board
     def setup_game
-      # set game mode
       game_mode
-      # set player profile
+
       player_setup
       greet(p1, p2)
-      # set game board
+
       generate_board
       winning_combos
       print_board
-      # enter game loop
-      game_loop
+
+      play
     end
 
-    # core game loop
-    def game_loop
+    # Play a game session
+    def play
       toss_a_coin
       play_turn until has_winner || remaining_slots.zero?
       end_game
@@ -69,23 +64,18 @@ module ConsoleGame
       input.std_show("connect4.pregame.msg2")
       @p1_turn = [true, false].sample
 
-      sleep(0.8)
-      input.print_msg(F.s("connect4.pregame.msg3", { player: p1_turn ? p1.name : p2.name }), pre: "* ")
+      input.print_msg(F.s("connect4.pregame.msg3", { player: p1_turn ? p1.name : p2.name }), pre: "* ", delay: 0.8)
     end
 
-    # Play turn
+    # Play turn (Core loop)
     def play_turn
       player = p1_turn ? p1 : p2
-      input.print_msg(F.s("connect4.turn.msg1", { player: player.name }), mode: :print)
-
       user_col = player_choice(player)
 
       valid_move, _user_pos = update_board(player, user_col)
-
       self.has_winner = four_in_a_row?(player)
 
       print_board
-
       self.p1_turn = !p1_turn if valid_move && !has_winner
     end
 
@@ -93,50 +83,23 @@ module ConsoleGame
     # @param player [ConsoleGame::Player, ConsoleGame::Computer]
     # @return [Integer]
     def player_choice(player)
-      value = nil
+      input.print_msg(F.s("connect4.turn.msg1", { player: player.name }), mode: :print)
       if player.is_a?(Computer)
-        value = player.random_move(empty_slots, bound)
-        print value
-        puts
+        player.random_move(empty_slots, bound)
       else
-        value = input.handle_input(reg: F.rs("connect4.turn.reg"), err_msg: F.s("connect4.turn.err")).to_i
+        input.handle_input(reg: F.rs("connect4.turn.reg"), err_msg: F.s("connect4.turn.err")).to_i
       end
-      value
     end
 
     # Generate game board as array
     def generate_board
-      @bound = [7, 6]
-      row, col = bound
-      @board = Array.new(col) { Array.new(row, e_slot) }
-    end
-
-    # calculate all winning combinations
-    def winning_combos
-      @combinations = {}
-      bound.reduce(:*).times do |idx|
-        combinations[idx] = valid_sequences(idx)
-      end
-      # p combinations
-    end
-
-    # Calculate valid sequence based on positional value
-    # @param value [Integer] positional value within a matrix
-    # @return [Array<Array<Integer>>] an array of valid directional path within given bound
-    def valid_sequences(value = 0)
-      arr = []
-      DIRECTIONS.each_key do |path|
-        sequence = direction(value, path)
-        arr << sequence unless sequence.empty?
-      end
-      arr
+      @board = Array.new(bound[1]) { Array.new(bound[0], e_slot) }
     end
 
     # count remaining slots
     def remaining_slots
-      @empty_slots = []
-      board.flatten.each_with_index { |elem, idx| empty_slots.push(idx) if elem == e_slot }
-
+      filled = p1.data.fetch(:moves) + p2.data.fetch(:moves)
+      @empty_slots = combinations.keys - filled
       empty_slots.size
     end
 
@@ -179,22 +142,10 @@ module ConsoleGame
       puts
     end
 
-    # Validate if current player has four in a row
-    # @param [ConsoleGame::Player, ConsoleGame::Computer] Player or Computer class object
-    # @return [Boolean]
-    def four_in_a_row?(player)
-      return false if player.turn < 4
-
-      moves = player.moves
-      combinations[moves.last].each do |sequence|
-        return true if sequence - moves == []
-      end
-      false
-    end
-
+    # End game handling
     def end_game
       super(nil)
-      msg = p1_turn ? "#{p1.name} has won in #{p1.turn} moves!" : "#{p2.name} has won in #{p2.turn} moves!"
+      msg = p1_turn ? "#{p1.name} has won in #{p1.data[:turn]} moves!" : "#{p2.name} has won in #{p2.data[:turn]} moves!"
       puts msg
     end
 
@@ -209,12 +160,11 @@ module ConsoleGame
     # @param player [ConsoleGame::Player] player class object
     # @return [ConsoleGame::Player, ConsoleGame::Computer]
     def player_profile(player)
-      if player.nil?
-        player = mode == 1 ? Player.new(game_manager, "") : Computer.new(game_manager)
-        return player if player.is_a?(Computer)
-      end
+      player ||= mode == 1 ? Player.new(game_manager, "") : Computer.new(game_manager)
+
+      return player if player.is_a?(Computer)
+
       player.edit_name(input.handle_input(F.s("connect4.name_player", { player: player.edit_name }), allow_empty: true))
-      player.clear_session(:moves)
       player
     end
 
